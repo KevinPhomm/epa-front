61f4b4b9995628eba67f2a623eb756c83762d737
Slawootsky <slawootsky@gmail.com>
2018-03-13 18:52:49 +0100

    Mes modifications 13 mars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EPA_logo from "./ressources/EPA_logo.png"
+import search_icon from "./ressources/search.png";
+import checkbox_icon from "./ressources/checkbox.png";
+import download_icon from "./ressources/download.png";
+import view_icon from "./ressources/view.png";
+import logout_icon from "./ressources/logout.png";
+import folder_icon from "./ressources/folder.png";
+import video_icon from "./ressources/video.png";
+import pdf_icon from "./ressources/pdf.png";
+import doc_icon from "./ressources/doc.png";
+import doc_big_icon from "./ressources/doc_big.png";
+import excel_icon from "./ressources/xls.png"
 
 class App extends Component {
 
@@ -48,11 +60,30 @@ class App extends Component {
     let column = []
 
     this.state.data.forEach((line,index)=>{
+
+      let icon = "";
+      if (line.type==="dir"){
+        icon=folder_icon
+      }
+      // Icône dossier
+      else if (line.type===""){
+        icon=doc_icon
+      }
+      // Icône doc (Word)
+      else if (line.type===""){
+        icon=excel_icon
+      }
+      // Icône xls (Excel)
+      else if (line.type===""){
+        icon=pdf_icon
+      }
+      // Icône pdf
+
       column.push(
         <div className="line" onClick={()=>this.openContent(index)}>
-          <img className="icon" src={line.type === "dir" ?  "http://www.icone-png.com/png/37/36873.png" : "https://sendeyo.com/images/file.png"}/>
+          <input className="checkbox" type="checkbox"/>
+          <img className="icon" src={icon} />
 
-          <input type="checkbox"/>
           <h3> {line.name} </h3>
         </div>
       )
@@ -63,8 +94,8 @@ class App extends Component {
     this.state.columntwo.forEach((line,index)=>{
       columntwo.push(
         <div className="line" onClick={()=>this.openFile(line)}>
+          <input className="checkbox" type="checkbox"/>
           <img className="icon" src={line.type === "dir" ?  "http://www.icone-png.com/png/37/36873.png" : "https://sendeyo.com/images/file.png"}/>
-          <input type="checkbox"/>
           <h3> {line.name} </h3>
         </div>
       )
@@ -75,25 +106,31 @@ class App extends Component {
     if (this.state.file !== undefined ){
       file =
       <div className="filezoom">
-      <img className="picto" src="https://sendeyo.com/images/file.png"/>
-      <h3> {this.state.file.name} </h3>
-      <button>Ouvrir</button>
-      <button>Telecharger</button>
+        <div>
+          <img className="picto" src="https://sendeyo.com/images/file.png"/>
+          <h3> {this.state.file.name} </h3>
+        </div>
+        <div>
+          <button><img src={view_icon}/> Ouvrir</button>
+          <br/>
+          <button><img src={download_icon}/> Telecharger</button>
+        </div>
       </div>
     }
 
     return (
       <div className="App">
         <div className="header">
-          <img className=" logo" src="http://www.cegid-education.com/wp-content/uploads/2017/01/Logo-EPA-AURA-Q.png"/>
-          <button className="logout">Se déconnecter</button>
+          <img className=" logo" src={EPA_logo}/>
+          <button className="logout"><img src={logout_icon}/>Se déconnecter</button>
         </div>
         <div className="column">{column}</div>
-        <div className={this.state.columntwo.length !== 0? "column greyer" : "empty-column"}>{columntwo}</div>
-        <div className={this.state.file !== undefined ? "column greyeryer" : "empty-column"}>{file}</div>
+        <div className={this.state.columntwo.length !== 0? "column column2" : "empty-column"}>{columntwo}</div>
+        <div className={this.state.file !== undefined ? "column column3" : "empty-column"}>{file}</div>
       </div>
     );
   }
+
 
 
 
